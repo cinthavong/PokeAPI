c0c2367b12e34b75dbb2b8e3221db2abbbbbbeab
Chelsea <cinthavong@gmail.com>
2023-10-19 23:12:15 -0700

    Updated commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+
 }
 
 android {
     namespace = "com.example.pokeapi"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "com.example.pokeapi"
@@ -37,9 +38,9 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:8.1.0")
+    implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.codepath.libraries:asynchttpclient:2.2.0")
     implementation("com.github.bumptech.glide:glide:4.12.0")
--- app/src/main/java/com/example/pokeapi/MainActivity.kt
+++ app/src/main/java/com/example/pokeapi/MainActivity.kt
@@ -1,22 +1,108 @@
 package com.example.pokeapi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
 
 class MainActivity : AppCompatActivity() {
+
     var pokeImageURL = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val pokeButton = findViewById<Button>(R.id.pokeButton)
+        val imageView = findViewById<ImageView>(R.id.pokeImage)
+
+        getNextImage(pokeButton, imageView)
+
+//        pokeButton.setOnClickListener {
+//        getMewPhoto()
+    }
+
+    private fun getDittoPhotoURL() {
+        val client = AsyncHttpClient()
+
+
+        client["https://pokeapi.co/api/v2/pokemon/ditto/", object :
+            JsonHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
+                Log.d("sprites", "response successful$json")
+                // Access a JSON array response with json.jsonArray
+                //Log.d("DEBUG ARRAY", json.jsonArray.toString())
+                // Access a JSON object response with 'json.jsonObject'
+                //Log.d("DEBUG OBJECT", json.jsonObject.toString())
+
+                pokeImageURL = json.jsonObject.getString("front_default")
+
+                //Log.d("pokeImageURL", pokeImageURL)
+            }
+
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers,
+                errorResponse: String,
+                throwable: Throwable
+            ) {
+                Log.d("Mew Error", errorResponse)
+            }
+        }]
+    }
+
+    private fun getSpritePhotoURL() {
+        val client = AsyncHttpClient()
+
+        client["https://pokeapi.co/api/v2/pokemon/ditto/", object :
+            JsonHttpResponseHandler() {
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Headers,
+                json: JsonHttpResponseHandler.JSON
+            ) {
+                //Log.d("DEBUG ARRAY", json.jsonArray.toString())
+                // Access a JSON object response with 'json.jsonObject'
+
+//                        Log.d("sprites", json.jsonArray.toString())
+//
+//                        val resultsJSON = json.jsonArray.getJSONObject(0)
+//                        pokeImageURL = resultsJSON.getString("back_female")
+//                        Log.d("sprites", pokeImageURL)
+
+                Log.d("sprites", "response successful$json")
+                if (json != null) {
+                    json.jsonObject.getJSONObject("sprites").getString("back_shiny")
+                }
+
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers,
+                errorResponse: String,
+                throwable: Throwable
+            ) {
+                Log.d("Sprite Error", errorResponse)
+            }
+        }]
+    }
+
 
     private fun getNextImage(button: Button, imageView: ImageView) {
         button.setOnClickListener {
-            getPokePhoto()
+            val choice = kotlin.random.Random.nextInt(2)
+
+            if (choice == 0) {
+                getDittoPhotoURL()
+            } else {
+                getSpritePhotoURL()
             }
 
             Glide.with(this)
@@ -26,44 +112,12 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
-        val pokeButton = findViewById<Button>(R.id.pokeButton)
-        pokeButton.setOnClickListener {
-            getPokePhoto()
-        }
+    }
 }
 
-    private fun getPokePhoto() {
-        val client = AsyncHttpClient()
 
 
-        client["https://pokeapi.co/api/v2/pokemon-species/mew", object :
-            JsonHttpResponseHandler() {
-            override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
-                Log.d("Poke", "response successful$json")
-                // Access a JSON array response with json.jsonArray
-                Log.d("DEBUG ARRAY", json.jsonArray.toString())
-                // Access a JSON object response with 'json.jsonObject'
-                Log.d("DEBUG OBJECT", json.jsonObject.toString())
 
-                val pokeImageURL = json.jsonObject.getString("message")
-                Log.d("pokeImageURL", pokeImageURL)
-                }
 
-                override fun onFailure(
-                    statusCode: Int,
-                    headers: Headers?,
-                    errorResponse: String,
-                    throwable: Throwable?
-                ) {
-                    Log.d("Poke Error", errorResponse)
-                }
-            }]
 
-}}
+
